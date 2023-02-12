6c76fadd4bfcbf2dd3a2f868716b7e22784d9144
ManojTGN <manoj.thunderviz@gmail.com>
2023-02-12 15:25:58 +0530

    complete aboutme section

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -19,61 +19,19 @@ document.querySelectorAll('.hypetext').forEach(function(hypetext) {
     };
     
 });
-
+let cont = document.getElementsByClassName("container")[0];
 document.querySelectorAll(".page-selector").forEach(function(pageSelector) {
-    let cont = document.getElementsByClassName("container")[0];
+    
     pageSelector.addEventListener('change', function() {
-        if(this.value == 0){
-            cont.scroll({ left: 0,behavior: 'smooth'});
-        }else if(this.value == 1){
-            cont.scroll({ left: parseInt( screen.width / 2),behavior: 'smooth'});
-        }else if(this.value == 2){
-            cont.scroll({ left: cont.scrollWidth / 1,behavior: 'smooth'});
-        }
+        if(this.value == 0)      cont.scroll({ top:0,left: 0,behavior: 'smooth'});
+        else if(this.value == 1) cont.scroll({ top:0,left: parseInt(screen.width/1.5),behavior: 'smooth'});
+        else if(this.value == 2) cont.scroll({ top:0,left: cont.scrollWidth / 1,behavior: 'smooth'});
     });
     
 });
 
-// let isMouseDown = false;
-// document.querySelectorAll('.top-box').forEach(function(topBox) {
-    
-//     let cont = document.getElementsByClassName("container")[0];
-
-//     topBox.onmousedown = event => {
-//         isMouseDown = true;
-//     };
-
-//     topBox.onmousemove = event => {
-//         if(!isMouseDown) return;
-        
-//         cont.scroll({
-//             left: parseInt( (event.clientX/cont.scrollWidth /*screen.width*/)  * cont.scrollWidth),
-//             behavior: 'auto'
-//         });
-
-//         let scroll = (event.clientX/cont.scrollWidth) * 100;
-//         let pageSelector = document.querySelectorAll(".page-selector");
-//         if     (scroll <  33.3) pageSelector[0].checked = true;
-//         else if(scroll >= 66.6) pageSelector[1].checked = true;
-//         else if(scroll >= 33.3) pageSelector[2].checked = true;
-//         console.log(scroll,cont.scrollLeft,screen.width,cont.scrollWidth);
-        
-//     }
-
-//     topBox.onmouseup = event => {
-//         isMouseDown = false;
-//     }
-    
-// });
-
-
-
-
 let index = 0,interval = 1000;
-
-const rand = (min, max) => 
-  Math.floor(Math.random() * (max - min + 1)) + min;
-
+const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 const animate = star => {
   star.style.setProperty("--star-left", `${rand(-10, 100)}%`);
   star.style.setProperty("--star-top", `${rand(-40, 80)}%`);
@@ -86,11 +44,9 @@ const animate = star => {
 for(const star of document.getElementsByClassName("magic-star")) {
   setTimeout(() => {
     animate(star);
-    
     setInterval(() => animate(star), 1000);
   }, index++ * (interval / 3))
 }
-
 
 
 document.getElementById("cards").onmousemove = e => {
@@ -98,7 +54,6 @@ document.getElementById("cards").onmousemove = e => {
       const rect = card.getBoundingClientRect(),
             x = e.clientX - rect.left,
             y = e.clientY - rect.top;
-  
       card.style.setProperty("--mouse-x", `${x}px`);
       card.style.setProperty("--mouse-y", `${y}px`);
     };
